ef59cfda16fee9f4ab43c5d0d6d7c1b076ed60e7
Abhishek Shakya <62395382+Mr-Paradox-as@users.noreply.github.com>
2023-08-07 18:07:17 +0000

    session

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "cart",
     "store",
     "django.contrib.admin",
     "django.contrib.auth",
@@ -64,6 +65,7 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
                 "store.views.categories", #updated
+                "cart.context_processors.cart",
             ],
         },
     },
--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -6,7 +6,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+
     path('',include('store.urls')),
+    
+    path('cart/',include('cart.urls')),
 
 ]
 
